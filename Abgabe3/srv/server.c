ecb0a681ed5737dc91f6a306b0f9893133837366
Tilman Dreßler <t.dre@kithara.com>
2022-06-12 23:42:53 +0200

    code beauty, new function try_

--- Abgabe3/srv/server.c
+++ Abgabe3/srv/server.c
@@ -8,6 +8,8 @@
 
 #define PORT 9000
 
+int try_(int r, char * err) {if (r == -1) { perror(err); exit(1); } return r; }
+
 static inline void die(const char* msg)
 {
 	perror(msg);
@@ -27,31 +29,26 @@ int main()
 	srv_addr.sin_port = htons(PORT);
 	srv_addr.sin_addr.s_addr = INADDR_ANY;
 
-	if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		die("Couldn't open the socket");
+	try_ ((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0), "Couldn't open the socket");
 
 	setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, (char*)&sockopt, sizeof(sockopt));
 
-	if (bind(sfd, (struct sockaddr*)&srv_addr, sad_sz) < 0)
-		die("Couldn't bind socket");
+	try_(bind(sfd, (struct sockaddr*)&srv_addr, sad_sz) < 0), "Couldn't bind socket");
 
-	if (listen(sfd, 1) < 0)
-		die("Couldn't listen to the socket");
+	try_(listen(sfd, 1) < 0), "Couldn't listen to the socket");
+	while (true) {
+		try_(accept(sfd, (struct sockaddr *)&cli_addr, &sad_sz), "Couldn't accept incoming connection");
+		while ((bytes = read(cfd, buf, sizeof(buf))) != 0)
+		{
+			if (bytes < 0)
+				die("Couldn't receive message");
 
-	cfd = accept(sfd, (struct sockaddr*)&cli_addr, &sad_sz);
-	if (cfd < 0)
-		die("Couldn't accept incoming connection");
+			if (write(cfd, buf, bytes) < 0)
+				die("Couldn't send message");
+		}
 
-	while ((bytes = read(cfd, buf, sizeof(buf))) != 0)
-	{
-		if (bytes < 0)
-			die("Couldn't receive message");
-
-		if (write(cfd, buf, bytes) < 0)
-			die("Couldn't send message");
+		close(cfd);
+		close(sfd);
 	}
-
-	close(cfd);
-	close(sfd);
 	return 0;
 }
